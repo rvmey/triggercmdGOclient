d43557172be74aaa924dd6cb6f10dd430f1d8457
Russell VanderMey <russell@vandermey.us>
2026-03-22 11:06:17 -0400

    Make arm6 binary for RaspPi 0 compatibility

--- build.ps1
+++ build.ps1
@@ -1,8 +1,18 @@
 $oses = @("windows", "darwin", "linux")
-$arches = @("amd64", "386", "arm")
+$arches = @("amd64", "386", "arm", "arm7")
 foreach ($os in $oses) {
     foreach ($arch in $arches) {
-        if (($arch -eq "arm" -and $os -eq "linux") -or ($arch -ne "arm")) {
+        $goarch = $arch
+        $goarm = $null
+        if ($arch -eq "arm7") {
+            $goarch = "arm"
+            $goarm = "7"
+        } elseif ($arch -eq "arm") {
+            $goarm = "6"
+        }
+
+        $isArmVariant = ($arch -eq "arm" -or $arch -eq "arm7")
+        if (($isArmVariant -and $os -eq "linux") -or (-not $isArmVariant)) {
             # Delete any existing binary
             if ($os -eq "windows") {
                 if (Test-Path binaries\tcmd-$os-$arch.exe -PathType Leaf) {
@@ -16,7 +26,8 @@ foreach ($os in $oses) {
 
             # Build the new binary
             $env:GOOS = $os
-            $env:GOARCH = $arch
+            $env:GOARCH = $goarch
+            if ($goarm) { $env:GOARM = $goarm } else { Remove-Item Env:GOARM -ErrorAction SilentlyContinue }
             go build -v -o binaries\tcmd-$os-$arch
 
             # Rename to .exe if it's windows
@@ -26,5 +37,13 @@ foreach ($os in $oses) {
         }        
     }
 }
+Remove-Item Env:GOARM -ErrorAction SilentlyContinue
+$signtool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe"
+$signArgs = @("/sha1", "4514d960dbb0b24571b642c38cfa439b67e7c738", "/tr", "http://timestamp.sectigo.com", "/td", "sha256", "/fd", "sha256", "/n", "VanderMey Consulting LLC")
+$windowsExes = Get-ChildItem -Path .\binaries\tcmd-windows-*.exe
+foreach ($exe in $windowsExes) {
+    & $signtool sign @signArgs $exe.FullName
+}
+
 Copy-Item .\binaries\tcmd-windows-amd64.exe .\binaries\tcmd.exe
 Copy-Item .\binaries\tcmd-windows-386.exe .\binaries\tcmd_32bit.exe
